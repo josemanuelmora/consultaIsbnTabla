920d8e0610f3f91b88037bacc1bbfa53ab9578d3
José Manuel Mora <josema81@hotmail.com>
2016-03-26 21:28:44 -0600

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by José Manuel Mora on 26/03/16.
+//  Copyright © 2016 José Manuel Mora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
